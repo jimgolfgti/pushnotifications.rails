c5eaa37d8d3a4ba8e0a76b520a40d7b23c661efb
jimgolfgti <jimgolfgti@gmail.com>
2013-03-03 16:37:39 +0000

    Initial application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PushnotificationsRails::Application.config.secret_token = '80c8b970b7671b0c6cd8aac35e2147600896e1d5ccb4b039a08afbbeeb1ac8a6e8a9f834f05f949d787ea98407ad9c3370a932544dafda62c7006678d41b8c59'
