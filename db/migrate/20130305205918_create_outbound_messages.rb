2b8b6bb5c9b2648bc6dda5de3d6b67dbe97f78a8
jimgolfgti <jimgolfgti@gmail.com>
2014-04-09 00:30:56 +0100

    Fixes issue with migration

--- db/migrate/20130305205918_create_outbound_messages.rb
+++ db/migrate/20130305205918_create_outbound_messages.rb
@@ -1,7 +1,6 @@
 class CreateOutboundMessages < ActiveRecord::Migration
   def change
     create_table :outbound_messages do |t|
-      t.primary_key :id
       t.string :message_id, limit: 36, null: false
       t.datetime :delivered_at
       t.datetime :failed_at
--- db/schema.rb
+++ db/schema.rb
@@ -9,33 +9,33 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130305223643) do
+ActiveRecord::Schema.define(version: 20130305223643) do
 
-  create_table "inbound_messages", :id => false, :force => true do |t|
-    t.string   "notification_id", :limit => 36, :null => false
-    t.string   "message_id",      :limit => 36, :null => false
-    t.string   "account_id",      :limit => 36, :null => false
-    t.text     "message_text",                  :null => false
-    t.string   "from",            :limit => 20, :null => false
-    t.string   "to",              :limit => 20, :null => false
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
+  create_table "inbound_messages", id: false, force: true do |t|
+    t.string   "notification_id", limit: 36, null: false
+    t.string   "message_id",      limit: 36, null: false
+    t.string   "account_id",      limit: 36, null: false
+    t.text     "message_text",               null: false
+    t.string   "from",            limit: 20, null: false
+    t.string   "to",              limit: 20, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "inbound_messages", ["account_id"], :name => "index_inbound_messages_on_account_id"
-  add_index "inbound_messages", ["notification_id", "message_id"], :name => "index_inbound_messages_on_notification_id_and_message_id", :unique => true
-  add_index "inbound_messages", ["to"], :name => "index_inbound_messages_on_to"
+  add_index "inbound_messages", ["account_id"], name: "index_inbound_messages_on_account_id"
+  add_index "inbound_messages", ["notification_id", "message_id"], name: "index_inbound_messages_on_notification_id_and_message_id", unique: true
+  add_index "inbound_messages", ["to"], name: "index_inbound_messages_on_to"
 
-  create_table "outbound_messages", :force => true do |t|
-    t.string   "message_id",   :limit => 36, :null => false
+  create_table "outbound_messages", force: true do |t|
+    t.string   "message_id",   limit: 36, null: false
     t.datetime "delivered_at"
     t.datetime "failed_at"
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "outbound_messages", ["message_id"], :name => "index_outbound_messages_on_message_id", :unique => true
+  add_index "outbound_messages", ["message_id"], name: "index_outbound_messages_on_message_id", unique: true
 
 end
